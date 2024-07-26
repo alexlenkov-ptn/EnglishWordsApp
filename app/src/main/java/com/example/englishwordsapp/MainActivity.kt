d784ae70f2cd854ff678c6534290f7092c2249ee
alexlenkov-ptn <alex.lenkov123@outlook.com>
2024-07-26 17:26:41 +0300

    added ViewBinding

--- app/src/main/java/com/example/englishwordsapp/MainActivity.kt
+++ app/src/main/java/com/example/englishwordsapp/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.example.englishwordsapp
 
+import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.example.englishwordsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+//    private lateinit var binding: ActivityMainBinding
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("Binding for ActivityLearnWordBinding ust " +
+                "not be null")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+//        val tvQuestionWord: TextView = findViewById(R.id.tvQuestionWord)
+//        tvQuestionWord.text = "42"
+//        tvQuestionWord.setTextColor(Color.BLUE)
+//        tvQuestionWord.setTextColor(Color.parseColor("#FDD600"))
+//        tvQuestionWord.setTextColor(ContextCompat.getColor(this, R.color.textVariantsColor))
+
+//        binding.tvQuestionWord.text = "AndroidSprint.ru"
+//        binding.tvQuestionWord.setTextColor(Color.GRAY)
+//        binding.imageButton.isVisible = false
+
+        with(binding) {
+            tvQuestionWord.text = "AndroidSprint.ru"
+            tvQuestionWord.setTextColor(Color.GRAY)
+            imageButton.isVisible = false
         }
     }
 }
